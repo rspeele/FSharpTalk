7a90642896dcc58ce116631fc0e0b3afe0ff51a5
Robert Peele <robert.s.peele@gmail.com>
2017-02-21 23:29:37 -0500

    Add screen scraping type provider example.

--- FSharpTalk/TypeProviderCSV.fsx
+++ FSharpTalk/TypeProviderCSV.fsx
@@ -1,6 +1,7 @@
-﻿#I "../packages/FSharp.Data.2.3.2/lib/net40"
-#r "FSharp.Data.DesignTime.dll"
-#r "FSharp.Data.dll"
+﻿#r "../packages/FSharp.Data.2.3.2/lib/net40/FSharp.Data.DesignTime.dll"
+#r "../packages/FSharp.Data.2.3.2/lib/net40/FSharp.Data.dll"
+// ^^ ignore these -- normally you would specify references in the project file, just like C#.
+// Writing them in code like this is only used with the interactive window.
 
 open FSharp.Data
 open System
@@ -12,12 +13,12 @@ open System.IO
 type Stocks = CsvProvider<"sample.csv">
 
 let run () =
-    let exampleText =
+    let exampleText = // CSV data as a string for this example, but it could come from anywhere at runtime.
         """Date,Open,High,Low,Close,Volume,Adj Close
     2017-02-21,64.610001,64.949997,64.449997,64.489998,19384900,64.489998
     2017-02-17,64.470001,64.690002,64.300003,64.620003,21234600,64.620003
     2017-02-16,64.739998,65.239998,64.440002,64.519997,20524700,64.519997"""
-    let stocks = Stocks.Load(new StringReader(exampleText)) // parse CSV file at runtime using generated type
+    let stocks = Stocks.Load(new StringReader(exampleText))
 
     for row in stocks.Rows do
         let date = row.Date // access parsed data in a statically typed way (this is a DateTime)
---
+++ FSharpTalk/TypeProviderHTML.fsx
@@ -0,0 +1,24 @@
+﻿#I "../packages/FSharp.Data.2.3.2/lib/net40"
+#r "FSharp.Data.DesignTime.dll"
+#r "FSharp.Data.dll"
+
+open FSharp.Data
+open System
+open System.IO
+
+// Generate type at compile time from the HTML on Wikipedia
+type CSharpArticle = HtmlProvider<"https://en.wikipedia.org/wiki/C_Sharp_(programming_language)">
+
+let run () =
+    // Load the article at runtime using the generated type
+    let page = CSharpArticle.Load("https://en.wikipedia.org/wiki/C_Sharp_(programming_language)")
+    // Scrape stuff out of the HTML in a type-safe way
+    let versionsTable = page.Tables.Versions
+    for row in versionsTable.Rows do
+        let date = row.Date // datetime
+        let version = row.Version // string
+        let net = row.``.NET Framework`` // string
+        printfn "%O %s %s" date version net
+
+// This is brittle -- if they significantly change the way the page is structured, our program will break.
+// But that is the nature of scraping, and at least we will find out when we build instead of when we run.
